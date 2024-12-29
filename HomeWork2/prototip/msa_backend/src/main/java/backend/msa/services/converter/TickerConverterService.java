a91dec75b8b8fcaaf88e42969143d4da1952c58d
anabaleska <anabaleska5@gmail.com>
2024-12-29 11:01:40 +0100

    Some changes

--- HomeWork2/prototip/msa_backend/src/main/java/backend/msa/services/converter/TickerConverterService.java
+++
@@ -1,8 +0,0 @@
-package backend.msa.services.converter;
-
-import backend.msa.model.Ticker;
-import backend.msa.model.dto.TickerDTO;
-
-public interface TickerConverterService {
-    TickerDTO convertToTickerDTO(Ticker ticker);
-}
